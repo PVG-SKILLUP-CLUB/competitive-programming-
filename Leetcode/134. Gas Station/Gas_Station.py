760018690def7bb79b1c6c934fa6b9c7b9c161b9
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2023-01-07 18:59:58 +0530

    Create Gas_Station.py

---
+++ Leetcode/134. Gas Station/Gas_Station.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        if sum(gas)<sum(cost):
+
+            return -1
+
+        res,total=0,0
+
+        for i in range(len(gas)):
+
+            total+=gas[i]-cost[i]
+
+            if total<0:
+
+                total=0
+
+                res=i+1
+
+        return res
