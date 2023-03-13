86ce0e74c33087a5ba465f22d6949a36558e03f4
ZAPatil <81514887+ZAPatil@users.noreply.github.com>
2023-03-13 21:43:18 +0530

    Create solution.cpp

---
+++ Leetcode/20. Valid Parentheses/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty())
+                    return false;
+                else{
+                    char c= st.top();
+                    if(s[i]==')' && c=='(' || s[i]==']' && c=='[' || s[i]=='}' && c=='{' )
+                        st.pop();
+                    else
+                        return false;
+                }
+            }
+        }
+        if(st.empty())
+                return true;
+            else
+                return false;
+    }
+};
