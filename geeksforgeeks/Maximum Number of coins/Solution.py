c208511341a826315ff278069c9cc540af057850
Jayesh More <65273915+jayesh-more@users.noreply.github.com>
2023-03-14 09:06:17 +0530

    Create Solution.py

---
+++ geeksforgeeks/Maximum Number of coins/Solution.py
@@ -0,0 +1,20 @@
+class Solution():
+    def maxCoins(self, N, a):
+        #your code goes here
+        MAX_N = N + 2
+        dp = [[0] * MAX_N for _ in range(MAX_N)]
+        a.insert(0, 1)
+        a.append(1)
+        for length in range(0, N - 1 + 1):
+            for start in range(1, N - length + 1):
+                end = start + length
+                max_profit = 0
+                for i in range(start, end + 1):
+                    profit = (
+                        dp[start][i - 1]
+                        + a[start - 1] * a[i] * a[end + 1]
+                        + dp[i + 1][end]
+                    )
+                    max_profit = max(max_profit, profit)
+                dp[start][end] = max_profit
+        return dp[1][N]
