ff375488486760f25ea41620393a5fbbfbad7ce8
ZAPatil <81514887+ZAPatil@users.noreply.github.com>
2023-03-16 22:41:27 +0530

    Create solution.cpp

---
+++ Leetcode/128. Longest Consecutive Sequence/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i=0; i<nums.size(); i++){
+            s.insert(nums[i]);
+        }
+        int ans=0;
+        for(int i=0; i<nums.size(); i++){
+            if(s.find(nums[i]-1)!=s.end()){
+                continue;
+            }
+        else{
+            int count = 0;
+            int current = nums[i];
+            while(s.find(current)!=s.end()){
+                count++;
+                current++;
+            }
+            ans = max(ans, count);
+        }
+        }
+        return ans;
+    }
+};
