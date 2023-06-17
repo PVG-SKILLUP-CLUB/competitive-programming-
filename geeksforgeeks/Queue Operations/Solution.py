98a41d41a070a8633040bf087cefc0659527dde0
Jayesh More <65273915+jayesh-more@users.noreply.github.com>
2023-06-17 22:32:39 +0530

    Create Solution.py

---
+++ geeksforgeeks/Queue Operations/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    def insert(self, q, k): 
+        #code here
+        q.append(k)
+        return q
+        
+    def findFrequency(self, q, k):
+        dict1 = {}
+        for i in q:
+            if(i not in dict1.keys()):
+                dict1[i] = 1
+            else:
+                dict1[i] += 1
+        if(k not in dict1.keys()):
+            return 0
+        else:
+            return dict1[k]
