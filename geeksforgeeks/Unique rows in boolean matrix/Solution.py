a4987656a9cf952d83b14e842820157f99453e0a
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2023-06-25 12:03:00 +0530

    Create Solution.py

---
+++ geeksforgeeks/Unique rows in boolean matrix/Solution.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+class Solution :
+    def uniqueRow(self,row, col, matrix):
+        #complete the function
+        result=[]
+        for i in matrix:
+            if i not in result:
+                result.append(i)
+        return result
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+    testcase = int(input())
+    while(testcase):
+        s = input().split()
+        row = int(s[0])
+        col = int(s[1])
+        matrix = [[None for _ in range(col)]for _ in range(row)]
+        s = input().split()
+        for i in range(row):
+            for j in range(col):
+                matrix[i][j] = int(s[i*col+j])
+        
+        ob = Solution()
+        a = ob.uniqueRow(row, col, matrix)
+        
+        for row in a:
+            for value in row:
+                print(value,end = " ")
+            print("$",end = "")
+        print()
+        testcase -= 1
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
