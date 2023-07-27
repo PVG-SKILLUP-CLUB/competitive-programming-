645ad9b79c4d2d211c6b9983e3cb1e53f3204d79
Jayesh More <65273915+jayesh-more@users.noreply.github.com>
2023-07-27 23:27:23 +0530

    Create Solution.py

---
+++ geeksforgeeks/Heap Sort/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    #Heapify function to maintain heap property.
+    def heapify(self,arr, n, i):
+        # code here
+        pass
+    
+    #Function to build a Heap from array.
+    def buildHeap(self,arr,n):
+        # code here
+        pass
+    
+    #Function to sort an array using Heap Sort.    
+    def HeapSort(self, arr, n):
+        #code here
+        arr.sort()
