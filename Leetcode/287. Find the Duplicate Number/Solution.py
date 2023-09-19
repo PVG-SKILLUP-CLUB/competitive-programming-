b32d1bc4af9607063cccc469a281c13fe2ced710
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2023-09-19 20:33:50 +0530

    Create Solution.py

---
+++ Leetcode/287. Find the Duplicate Number/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        num=set()
+        for n in nums:
+            if n in num:
+                return n
+            num.add(n)
